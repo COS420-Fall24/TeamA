5b6e56a17bf2e3a78a36abbad29fcb74d4ef0d6c
Drew Marecek <drewmarecek@host-0800-9c3e5377022a.pool.net.maine.edu>
2024-10-09 19:42:13 -0400

    Added a footer for the app. Made sure everything was working correctly. Did some minor bug fixes.

--- app/src/App.js
+++ app/src/App.js
@@ -1,5 +1,6 @@
 import logo from './logo.svg';
 import './App.css';
+import React from 'react';
 
 function App() {
   return (
@@ -18,6 +19,9 @@ function App() {
           Learn React
         </a>
       </header>
+      <footer className="App-footer">
+        <p>© 2024 EmpowerMaine. All rights reserved.</p>
+      </footer>
     </div>
   );
 }

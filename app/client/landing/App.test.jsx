bc226aa4928595eebed175b14ec70bed19e209e9
drewmarecek <124214087+drewmarecek@users.noreply.github.com>
2024-11-08 14:47:29 -0500

    Update App.test.jsx

--- app/client/landing/App.test.jsx
+++ app/client/landing/App.test.jsx
@@ -1,7 +1,7 @@
 import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import '@testing-library/jest-dom';
-import App from './Landing';
+import App from './App';
 
 describe('App Component', () => {
   it('renders the EmpowerMaine title and navigation links', () => {

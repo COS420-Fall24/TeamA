d0caf9cc5a5e860493000866be10dffcb0434d3a
drewmarecek <124214087+drewmarecek@users.noreply.github.com>
2024-11-08 14:35:26 -0500

    Update App.test.jsx

--- app/client/landing/App.test.jsx
+++ app/client/landing/App.test.jsx
@@ -1,6 +1,6 @@
-import { render, screen } from '@testing-library/react';
-import { MemoryRouter } from 'react-router-dom';
-import userEvent from '@testing-library/user-event';
+import React from 'react';
+import { render, screen, fireEvent } from '@testing-library/react';
+import '@testing-library/jest-dom';
 import App from './App';
 
 describe('App Component', () => {

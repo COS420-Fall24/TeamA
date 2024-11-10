ea4c7b7e2c1e82213730337262b954af7e3c4096
Jacob Bryant <jacob.bryant1@maine.edu>
2024-11-09 19:27:58 -0500

    Fix login to use firebase token

--- app/routes/api.js
+++ app/routes/api.js
@@ -1,10 +1,10 @@
 const express = require('express');
-const { register } = require('../controller/register');
-const { loginController } = require('../controller/checkLoginDetails');
+const { registerController } = require('../controller/register');
+const { loginController } = require('../controller/login');
 
 const router = express.Router();
 router.post('/login', loginController);
-router.post('/register', register);
+router.post('/register', registerController);
 
 module.exports = router;
 

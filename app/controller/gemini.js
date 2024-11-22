2cac9707555347b139d5d7369f2b615c408d7a66
Jacob Bryant <jacob.bryant1@maine.edu>
2024-11-21 21:02:16 -0500

    Fix AI to accept prompt from request

--- app/controller/gemini.js
+++ app/controller/gemini.js
@@ -4,13 +4,11 @@ const { GoogleGenerativeAI } = require("@google/generative-ai");
 const genAI = new GoogleGenerativeAI(process.env.GEMINI_KEY);
 const model = genAI.getGenerativeModel({ model: "gemini-1.5-flash" });
 
-async function processPrompt() {
+async function processPrompt(req, res) {
     try {
-        const prompt = "Hi gemini";
+        const prompt = req.body.prompt;
         const result = await model.generateContent([prompt]);
-
-        console.log(result.response.text());
-        return { message: result.response.text() };
+        return res.json({ message: result.response.text() });
     } catch (error) {
         console.error("Error with the API call:", error.message);
         throw error;

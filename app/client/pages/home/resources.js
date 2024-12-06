19af5bd330c0934454b40c99800a0047c45a0df8
Dominic <dominic.huntington@maine.edu>
2024-12-06 18:37:47 -0500

    resources added to home

---
+++ app/client/pages/home/resources.js
@@ -0,0 +1,34 @@
+export const resources = [
+  {
+      title: 'Interview Guide',
+      description: 'A guide to succeeding in technical interviews with practice questions',
+      image: '/path/to/placeholder-image.jpg'
+  },
+  {
+      title: 'Resume Builder',
+      description: 'Create a professional resume with our easy-to-use builder',
+      image: '/path/to/placeholder-image.jpg'
+  },
+  {
+      title: 'Career Paths',
+      description: 'Explore different career paths and opportunities in tech',
+      image: '/path/to/placeholder-image.jpg'
+  },
+  {
+      title: 'Networking Tips',
+      description: 'Learn how to build professional relationships effectively',
+      image: '/path/to/placeholder-image.jpg'
+  },
+  {
+      title: 'Skills Assessment',
+      description: 'Evaluate your technical and soft skills',
+      image: '/path/to/placeholder-image.jpg'
+  },
+  {
+      title: 'Job Search Strategy',
+      description: 'Develop an effective strategy for your job search',
+      image: '/path/to/placeholder-image.jpg'
+  }
+];
+
+export default resources;
